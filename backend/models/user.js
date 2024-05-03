3d84943c4f3f506ad8f90c966c7b422dd9e0d5a9
Anastasya Farikha <afar0036@student.monash.edu>
2024-05-04 05:01:25 +1000

    add background

---
+++ backend/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+var bcrypt = require('bcrypt-nodejs');
+
+const userSchema = mongoose.Schema({
+    username: {
+		type: String
+	},
+    password: {
+		type: String
+	},
+    name: {
+		type: String
+	},
+    score: {
+		type: Number
+	}
+});
+
+module.exports = mongoose.model("User", userSchema);
