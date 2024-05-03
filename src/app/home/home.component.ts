80652dffe8a1428a153c25c8dc389a568d3b0e3c
francisanthony17 <fant0003@student.monash.edu>
2024-05-04 04:17:29 +1000

    added calendar to hoepgae

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  days: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  weeks: number[][] = [];
 
+  constructor() {
+    this.generateCalendar();
+  }
+
+  generateCalendar() {
+    const currentDate = new Date();
+    const monthStart = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      1
+    );
+    const monthEnd = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      0
+    );
+    const weeks: number[][] = [];
+    let week: number[] = [];
+
+    for (let i = 1; i <= monthEnd.getDate(); i++) {
+      const date = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        i
+      );
+      week.push(i);
+      if (date.getDay() === 6 || i === monthEnd.getDate()) {
+        weeks.push(week);
+        week = [];
+      }
+    }
+    this.weeks = weeks;
+  }
 }
