65a612e1100b73f1e1524ab966446784bb2112a7
HanselLiebrata <hlie0011@student.monash.edu>
2024-05-04 01:52:49 +1000

    wasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreateTimeComponent } from './create-time/create-time.component';
 
 const routes: Routes = [
   { path: "test", component: HomeComponent, title: "Time" },
-  { path: '', redirectTo: '/test', pathMatch: 'full' }
+  { path: '', redirectTo: '/test', pathMatch: 'full' },
+  { path: "CreateTime", component: CreateTimeComponent, title: "Create Timetable" },
 ];
 
 @NgModule({
