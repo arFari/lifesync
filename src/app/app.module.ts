e86f83ba3233094661d0ad23aa18f756a7e1d62c
HanselLiebrata <hlie0011@student.monash.edu>
2024-05-04 15:34:59 +1000

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
 import { CreateTimeComponent } from './create-time/create-time.component';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
+import { DatabaseService } from './service/database.service';
+
+const routes: Routes = [
+  { path: "CreateTime", component: CreateTimeComponent, title: "Create Timetable" },
+  { path: "test", component: HomeComponent, title: "Time" },
+  { path: '', redirectTo: '/test', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
@@ -15,11 +23,12 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
+    RouterModule.forRoot(routes, { useHash: true }), 
+    HttpClientModule,
     CommonModule,
   ],
-  providers: [],
+  providers: [DatabaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
