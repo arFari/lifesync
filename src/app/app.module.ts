c59f8cfd643d821e506473de9506ffd6260c187c
Anastasya Farikha <afar0036@student.monash.edu>
2024-05-04 15:44:16 +1000

    resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreateTimeComponent } from './create-time/create-time.component';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
+import { DatabaseService } from './service/database.service';
+import { CollectiblesComponent } from './collectibles/collectibles.component';
+
+const routes: Routes = [
+  { path: "CreateTime", component: CreateTimeComponent, title: "Create Timetable" },
+  { path: "test", component: HomeComponent, title: "Time" },
+  { path: '', redirectTo: '/test', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CreateTimeComponent,
-    CollectiblesComponent,
   ],
   imports: [
     BrowserModule,
