765d8ba327f91e0717ec2ea0fc944b5a4969f66e
Anastasya Farikha <afar0036@student.monash.edu>
2024-05-05 10:38:01 +1000

    fixed add items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +10,26 @@ import { RegisterComponent } from './register/register.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) { }
   title = 'webhack';
   public showNavBar = true;
 
-  toggleNavBar(component:any) {
-    if(component instanceof LoginComponent) {
-       this.showNavBar = false;
-    } 
-    else if(component instanceof RegisterComponent){
+  toggleNavBar(component: any) {
+    if (component instanceof LoginComponent) {
+      this.showNavBar = false;
+    }
+    else if (component instanceof RegisterComponent) {
       this.showNavBar = false;
     }
     else {
-       this.showNavBar = true;
+      this.showNavBar = true;
     }
- }
- 
+  }
+
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
+
 }
 
